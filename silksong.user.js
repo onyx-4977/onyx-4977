1a8f912fc0e9d241be128061fd39921e950b88f3
onyx-4977 <102837433+onyx-4977@users.noreply.github.com>
2022-04-02 14:25:42 +0200

    Added slider to change opacity

--- silksong.user.js
+++ silksong.user.js
@@ -1,24 +1,42 @@
 // ==UserScript==
 // @name         Silksong Place
 // @namespace    https://www.reddit.com/r/HollowKnight/
-// @version      0.1
+// @version      0.2
 // @description  try to take over r/place!
 // @author       OnyX_#4977
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
-console.log("script");
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
         (function () {
             const div = document.createElement("div");
+            div.id = "Silksong";
             div.style = "height:3550px; width:4400px; position: absolute; left: 0; right:0; top: 0; bottom:0; transform: translateX(11350px) translateY(17350px);     background-size: cover;" +
     "background-image: url('https://cdn.discordapp.com/attachments/959559823089860638/959588823602458644/unknown.png'); opacity: 0.3;";
-            console.log(div);
             return div;
         })())
+
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByClassName("bottom-controls")[0].appendChild(
+        (function () {
+            const slider = document.createElement("div");
+            slider.style = "height: 36px; width: 200px; position: absolute;  right: 100px; top: 0;  background-color: #FFF;pointer-events: all;border-radius: 26px;";
+            const input = document.createElement("input");
+            input.type = 'range';
+            input.min = '0';
+            input.max = '1';
+            input.step = '0.1';
+            input.value = '0.3';
+            input.style = "margin: 10px;left: 0;right: 0;top: 0;bottom: 0;box-sizing: border-box;position: absolute;";
+            input.addEventListener('input', (event) => {
+                document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelector("#Silksong").style.opacity = event.currentTarget.value;
+            });
+            slider.appendChild(input);
+            return slider;
+        })()
+        );
 
     }, false);
 }
