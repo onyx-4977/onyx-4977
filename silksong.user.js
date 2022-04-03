f1bd40ff5a964c06ae9d10e24bed7658f25fdaa2
onyx-4977 <102837433+onyx-4977@users.noreply.github.com>
2022-04-03 17:57:05 +0200

    Update script with Radiance + Tree template

--- silksong.user.js
+++ silksong.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Silksong Place
 // @namespace    https://www.reddit.com/r/HollowKnight/
-// @version      0.5.6
+// @version      0.6.0
 // @description  try to take over r/place!
 // @author       OnyX_#4977
 // @match        https://hot-potato.reddit.com/embed*
@@ -13,8 +13,9 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
         (function () {
             const div = document.createElement("div");
+            div.className = "Template";
             div.id = "Silksong";
-            div.style = "height:3900px; width:5250px; position: absolute; inset: 0px; transform: translateX(11250px) translateY(17150px);     background-size: cover;" +
+            div.style = "height:3900px; width:5250px; position: absolute; inset: 0px; transform: translateX(11250px) translateY(17150px);     background-size: cover; image-rendering: pixelated;" +
     "background-image: url('https://raw.githubusercontent.com/onyx-4977/onyx-4977/main/SilksongTemplate.png'); opacity: 0.3;";
             return div;
         })())
@@ -22,11 +23,35 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
         (function () {
             const div = document.createElement("div");
+            div.className = "Template";
             div.id = "HollowKnight";
             div.style = "height:3500px; width:5600px; position: absolute; inset: 0px; transform: translateX(65000px) translateY(2500px);     background-size: cover; image-rendering: pixelated;" +
     "background-image: url('https://github.com/onyx-4977/onyx-4977/raw/main/HollowKnightTemplate.png'); opacity: 0.3;";
             return div;
         })())
+
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
+        (function () {
+            const div = document.createElement("div");
+            div.className = "Template";
+            div.id = "Radiance";
+            div.style = "height:5250px; width:4700px; position: absolute; inset: 0px; transform: translateX(30250px) translateY(82500px);     background-size: cover; image-rendering: pixelated;" +
+    "background-image: url('https://github.com/onyx-4977/onyx-4977/raw/main/RadianceTemplate.png'); opacity: 0.3;";
+            return div;
+        })())
+
+        /// BACKUP PLAN
+        /*
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(
+        (function () {
+            const div = document.createElement("div");
+            div.className = "Template";
+            div.id = "Radiance2";
+            div.style = "height:5050px; width:4550px; position: absolute; inset: 0px; transform: translateX(15250px) translateY(82500px);     background-size: cover; image-rendering: pixelated;" +
+    "background-image: url('https://github.com/onyx-4977/onyx-4977/raw/main/RadianceTemplate.png'); opacity: 0.3;";
+            return div;
+        })())
+        */
 
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByClassName("bottom-controls")[0].appendChild(
         (function () {
@@ -40,8 +65,7 @@ if (window.top !== window.self) {
             input.value = '0.3';
             input.style = "margin: 10px;left: 0;right: 0;top: 0;bottom: 0;box-sizing: border-box;position: absolute;";
             input.addEventListener('input', (event) => {
-                document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelector("#Silksong").style.opacity = event.currentTarget.value;
-                document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelector("#HollowKnight").style.opacity = event.currentTarget.value;
+                document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelectorAll(".Template").forEach(element => {element.style.opacity = event.currentTarget.value});
             });
             slider.appendChild(input);
             return slider;
