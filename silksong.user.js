7802f9dc260b7494a0ee7344071327264c9d217f
MilesZew <44184662+MilesZew@users.noreply.github.com>
2022-04-04 09:03:40 -0700

    set dots to be default

--- silksong.user.js
+++ silksong.user.js
@@ -17,7 +17,7 @@ function makeOverlay(id, url, width, height, x, y) {
     const div = document.createElement("div");
     div.className = "Template";
     div.id = id;
-    div.style = `height:${height}px; width:${width}px; position: absolute; inset: 0px; transform: translateX(${x}px) translateY(${y}px); background-size: cover; image-rendering: pixelated; background-image: url('${url}'); opacity: 0.3;`;
+    div.style = `height:${height}px; width:${width}px; position: absolute; inset: 0px; transform: translateX(${x}px) translateY(${y}px); background-size: cover; image-rendering: pixelated; background-image: url('${url}'); opacity: 1`;
     document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].appendChild(div);
 }
 
@@ -38,8 +38,9 @@ if (window.top !== window.self) {
             input.min = '0';
             input.max = '1';
             input.step = '0.1';
-            input.value = '0.3';
+            input.value = '1';
             input.style = "margin: 10px;left: 0;right: 0;top: 0;bottom: 0;box-sizing: border-box;position: absolute;";
+            input.id = 'templateSlider'
             input.addEventListener('input', (event) => {
                 document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelectorAll(".Template").forEach(element => {element.style.opacity = event.currentTarget.value});
             });
@@ -56,7 +57,6 @@ if (window.top !== window.self) {
             input.type = 'checkbox';
             input.name = "toggle";
             input.addEventListener('click', (event) => {
-                console.log(event.currentTarget.checked);
                 document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-camera")[0].shadowRoot.children[0].children[0].children[0].querySelectorAll(".Template").forEach(element => {
                     if (event.currentTarget.checked) {
                         element.style["-webkit-mask-image"] = `url("https://raw.githubusercontent.com/onyx-4977/onyx-4977/f648a8946a1fe2f37c7b6593d5c889ab7be95085/Dot.svg")`;
@@ -69,6 +69,7 @@ if (window.top !== window.self) {
                     }
                 });
             });
+            input.click()
             const label = document.createElement("label");
             label.style = "color: black;";
             label.innerHTML = "Toggle dot";
